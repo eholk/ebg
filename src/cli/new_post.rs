6f977f913384f29be92f07d41c5b44964acee9ed
Eric Holk <eric@theincredibleholk.org>
2023-05-22 18:39:40 -0700

    Add new-post command
    Adds a basic new-post command to the CLI. This command will create a new
    post in the posts directory with the current date and a slugified title.
    
    It's missing some more advanced features, like templates, but it'll
    still save time for making new posts.
    
    Fixes #8.

--- src/cli/new_post.rs
+++ src/cli/new_post.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use tokio::io::AsyncWriteExt;
+use tracing::debug;
 
 #[derive(Parser)]
 pub struct NewPostOptions {
@@ -7,7 +9,45 @@ pub struct NewPostOptions {
 
 impl super::Command for NewPostOptions {
     async fn run(self) -> eyre::Result<()> {
-        println!("Creating new post with title: {}", self.title);
+        // make sure there's a Site.toml in the current directory
+        let root = std::env::current_dir()?;
+        let site_toml = root.join("Site.toml");
+        if !site_toml.exists() {
+            return Err(eyre::eyre!(
+                "No Site.toml found in current directory: {}",
+                root.display()
+            ));
+        }
+
+        let posts_dir = root.join("_posts");
+
+        if !posts_dir.exists() {
+            tokio::fs::create_dir(&posts_dir).await?;
+        }
+
+        let post_filename = posts_dir.join(format!(
+            "{}-{}.md",
+            chrono::Local::now().format("%Y-%m-%d"),
+            slug::slugify(&self.title)
+        ));
+        debug!("creating new post at {}", post_filename.display());
+
+        let mut file = tokio::fs::File::create(&post_filename).await?;
+        file.write_all(
+            format!(
+                r#"---
+layout: post
+published: false
+---
+
+# {title}
+"#,
+                title = self.title
+            )
+            .as_bytes(),
+        )
+        .await?;
+
         Ok(())
     }
 }
